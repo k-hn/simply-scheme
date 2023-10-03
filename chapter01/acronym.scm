4befbed321ec58b6b7a5e9aa13e0ee9c62dc41f0
k-hn <125813796+k-hn@users.noreply.github.com>
2023-10-03 23:34:37 +0000

    initial commit

---
+++ chapter01/acronym.scm
@@ -0,0 +1,7 @@
+(include "../harvey/simply.scm")
+
+(define (acronym phrase)
+  (accumulate word (every first (keep real-word? phrase))))
+
+(define (real-word? wd)
+  (not (member? wd '(a the an in of and for to with))))
