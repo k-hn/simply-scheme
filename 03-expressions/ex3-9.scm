291c0f7caeea8240868f27bdff212b29f92dc3e7
k-hn <125813796+k-hn@users.noreply.github.com>
2023-10-05 18:58:42 +0000

    add chapter 3 exercises

---
+++ 03-expressions/ex3-9.scm
@@ -0,0 +1,14 @@
+;; An atom
+10
+
+;; Another compound expression made up of three atoms
+(+ 7 2 1)
+
+;; A compound expression made up of four atoms
+(+ 1 2 3 4)
+
+;; A compound expression made up of an atom adn two compound subexpressions
+(+ 7 (* 2 1) (* 1 1))
+
+;; Any other kind of expression
+(/ 10 1 1 1 1 1 1)
